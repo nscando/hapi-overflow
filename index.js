a138cfe8c54a1bba481a63875322a3f6fa083ca3
nscando <nicolas.scandizzo@gmail.com>
2020-11-23 10:19:44 -0300

    Public - Plugins - Statics

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
 'use strict'
-
 const Hapi = require('@hapi/hapi');
+const inert = require('inert');
+const path = require('path');
 
 const server = Hapi.server({
-     port: process.env.PORT || 3000,
-     host: 'localhost'
+  port: process.env.PORT || 3000,
+  host: 'localhost',
+  routes: {
+    files: {
+      relativeTo: path.join(__dirname, 'public')
+    }
+  }
 });
 
 async function init() {
-     server.route({
-          method: 'GET',
-          path: '/',
-          handler: (req, h) => {
-               return h.response('hola nico')
-          }
-     });
 
-     try {
-          await server.start()
-     } catch (error) {
-          console.error(error);
-          process.exit(1)
-     }
-     console.log(`Server listening on port: ${server.info.uri}`);
+  try {
+    await server.register(inert)
+
+    server.route({
+      method: 'GET',
+      path: '/home',
+      handler: (req, h) => {
+        return h.file('index.html')
+      }
+    });
+
+    server.route({
+      method: 'GET',
+      path: '/{param*}',
+      handler: {
+        directory: {
+          path: '.',
+          index: ['index.html']
+        }
+      }
+    });
+
+    await server.start()
+  } catch (error) {
+    console.error(error);
+    process.exit(1)
+  }
+  console.log(`Server listening on port: ${server.info.uri}`);
 };
 
 init();
